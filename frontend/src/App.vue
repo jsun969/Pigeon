0226de0b961a6acacb8f0e1e9bb14aa3a52c462c
jsun969 <i@jsun969.cn>
2021-03-14 23:48:06 +0800

    feat: add router in frontend
    - Add router in frontend
    - Modified login display ideas
    - The bottom navigation has style bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,21 +2,25 @@
   <v-app>
     <v-app-bar app color="primary" dark>飞鸽传书</v-app-bar>
     <v-main>
-      <UserAuth />
+      <UserAuth v-if="isLogin" />
+      <Main v-else />
     </v-main>
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld";
-import UserAuth from "./components/UserAuth";
-
+import UserAuth from './components/UserAuth';
+import Main from './components/Main';
 export default {
-  name: "App",
-
+  name: 'App',
   components: {
     // HelloWorld,
     UserAuth,
+    Main,
   },
+  data: () => ({
+    isLogin: false,
+  }),
 };
 </script>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <router-view />
+    <v-bottom-navigation :value="value" color="primary">
+      <router-link to="/">
+        <v-btn>
+          <span>Home</span>
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+      </router-link>
+      <router-link to="/about">
+        <v-btn>
+          <span>About</span>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </router-link>
+      <v-btn>
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+};
+</script>
