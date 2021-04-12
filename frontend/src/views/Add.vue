858f5c528311bfcb5858aada8d07e111e446c362
jsun969 <i@jsun969.cn>
2021-04-12 21:28:54 +0800

    feat(addPage): complete text field rules and button disabled events

--- frontend/src/views/Add.vue
+++ frontend/src/views/Add.vue
@@ -3,15 +3,27 @@
     <v-container>
       <v-row style="margin:4px 0px -36px">
         <v-col>
-          <v-text-field v-model="newCode" label="设备代码" :rules="codeRules" outlined></v-text-field>
+          <v-text-field
+            v-model="newCode"
+            label="设备代码"
+            :rules="[...codeRules, !devices.map(({ code }) => code).includes(+newCode) || !newCode || '设备已添加']"
+            @update:error="isAddingErr = $event"
+            outlined
+          ></v-text-field>
         </v-col>
         <v-col>
-          <v-text-field v-model="newName" label="备注名" outlined></v-text-field>
+          <v-text-field
+            v-model="newName"
+            label="备注名"
+            :rules="[!devices.map(({ name }) => name).includes(newName) || !newName || '备注名重复']"
+            @update:error="isAddingErr = $event"
+            outlined
+          ></v-text-field>
         </v-col>
       </v-row>
       <v-row style="padding:0px 8px 0px 8px">
         <v-col>
-          <v-btn block large color="primary">添加</v-btn>
+          <v-btn block large color="primary" :disabled="isAddingErr || !newName || !newCode">添加</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -31,7 +43,19 @@
             <td>{{ item.code }}</td>
             <td v-if="!item.editing">{{ item.name }}</td>
             <td v-if="item.editing" style="width:130px">
-              <v-text-field v-model="item.editingName"></v-text-field>
+              <v-text-field
+                v-model="item.editingName"
+                :rules="[
+                  !devices
+                    .map(({ name }) => name)
+                    .filter(val => val !== item.name)
+                    .includes(item.editingName) ||
+                    !item.editingName ||
+                    '备注名重复',
+                  !!item.editingName || '请输入备注',
+                ]"
+                @update:error="isEditingError = $event"
+              ></v-text-field>
             </td>
             <td>
               <v-btn icon color="green" :disabled="item.status === 2" v-if="!item.editing" @click="startEditing({ index })">
@@ -39,7 +63,9 @@
               </v-btn>
               <v-btn icon color="red" :disabled="item.status === 2" v-if="!item.editing"><v-icon>mdi-delete</v-icon></v-btn>
               <v-btn icon color="red" v-if="item.editing" @click="stopEditing({ index })"><v-icon>mdi-close</v-icon></v-btn>
-              <v-btn icon color="green" v-if="item.editing" @click="confirmEdit(item, index)"><v-icon>mdi-check</v-icon></v-btn>
+              <v-btn icon color="green" v-if="item.editing" @click="confirmEdit(item, index)" :disabled="isEditingError">
+                <v-icon>mdi-check</v-icon>
+              </v-btn>
             </td>
           </tr>
         </tbody>
@@ -56,6 +82,7 @@ export default {
   data: () => ({
     newCode: '',
     newName: '',
+    isAddingErr: false,
     codeRules: [
       value => (value || '').length <= 6 || !value || '最多6个字符',
       value => (value || '').length >= 6 || !value || '至少6个字符',
@@ -64,6 +91,7 @@ export default {
         return pattern.test(value) || !value || '设备代码应为纯数字';
       },
     ],
+    isEditingError: false,
   }),
   methods: {
     confirmEdit(item, index) {
