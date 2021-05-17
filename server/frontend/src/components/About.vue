4b0fe3b92aac0bc9783b9d60a634c0f922d20256
jsun969 <i@jsun969.cn>
2021-05-17 22:45:06 +0800

    feat(frontend): add about page in settings

---
+++ server/frontend/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="d-flex flex-column title">
+    <span>感谢您使用飞鸽传书</span>
+    <span>此为<a href="https://jsun969.cn">荆棘</a>的高中研究课项目</span>
+    <span>已在Github开源</span>
+    <span>项目地址: <a href="https://github.com/jsun969/Pigeon">jsun969/pigeon</a> </span>
+    <span>感谢您的Star</span>
+    <span>欢迎Fork帮助我完善这个项目</span>
+    <span>如果遇到问题 可在Github提出Issue</span>
+    <span>或联系作者邮箱 <a href="mailto:i@jsun969.cn">i@jsun969.cn</a> </span>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'About',
+};
+</script>
