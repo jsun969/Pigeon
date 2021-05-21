825daa95387102c2350f2696f9eace9e64cbc10d
jsun969 <i@jsun969.cn>
2021-05-21 23:42:50 +0800

    style(frontend): add more style and content in about

--- server/frontend/src/components/About.vue
+++ server/frontend/src/components/About.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-container class="d-flex flex-column title">
-    <span>感谢您使用飞鸽传书</span>
-    <span>此为<a href="https://jsun969.cn">荆棘</a>的高中研究课项目</span>
-    <span>已在Github开源</span>
-    <span>项目地址: <a href="https://github.com/jsun969/Pigeon">jsun969/pigeon</a> </span>
-    <span>感谢您的Star</span>
-    <span>欢迎Fork帮助我完善这个项目</span>
-    <span>如果遇到问题 可在Github提出Issue</span>
-    <span>或联系作者邮箱 <a href="mailto:i@jsun969.cn">i@jsun969.cn</a> </span>
+  <v-container class="d-flex flex-column">
+    <span class="title font-weight-bold">感谢您使用飞鸽传书</span>
+    <span>作者: <a href="https://jsun969.cn">荆棘Justin</a></span>
+    <span>Github开源地址: <a href="https://github.com/jsun969/Pigeon">jsun969/Pigeon</a> </span>
+    <span>欢迎Star和Fork</span>
+    <span class="title font-weight-bold">联系作者</span>
+    <span>邮箱: <a href="mailto:i@jsun969.cn">i@jsun969.cn</a> </span>
+    <span>QQ: 1351083056</span>
+    <span>微信/钉钉: jsun969</span>
   </v-container>
 </template>
 <script>
